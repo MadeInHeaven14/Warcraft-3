7ee84bf2dfc0c853837cd810887bcb5033e909b1
artur-nikiforenko@mail.ru <artur-nikiforenko@mail.ru>
2023-01-13 20:40:28 +0300

    Добавьте файлы проекта.

---
+++ Warcraft 3/Program.cs
@@ -0,0 +1,19 @@
+﻿using Warcraft_3;
+
+Mage mage = new Mage();
+Peasant peasant = new Peasant();
+Footman footman = new Footman();
+Archer archer = new Archer();
+BlackSmith blacksmith = new BlackSmith();
+
+archer.Attack(peasant);
+archer.Attack(peasant);
+archer.Attack(peasant);
+mage.TargetHeal(peasant);
+footman.Attack(archer);
+footman.Attack(archer);
+footman.Attack(archer);
+footman.Attack(archer);
+footman.Attack(archer);
+footman.Attack(archer);
+mage.TargetHeal(archer); 
